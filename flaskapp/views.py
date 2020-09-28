6b9e5d6575979c1ea370d390ec82c84362d2f52f
Andrew Emerick <aemerick11@gmail.com>
2020-09-28 09:59:01 -0700

    embedding folium map

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -3,7 +3,41 @@ from flaskapp import app
 # from flaskapp.a_model import ModelIt
 import pandas as pd
 from flask import request
+import folium
+from flask import Flask, Markup
+
 
 @app.route('/')
 def homepage():
-    return render_template("model_page.html")
+    start_coords = (-26.52, 153.09)
+    folium_map = folium.Map(location=start_coords, zoom_start=13, width='80%')
+
+    # Extract the components of the web map
+
+    #
+    #  The HTML to create a <div>  to hold the map
+    #
+    #  The header text to get styles, scripts, etc
+    #
+    #  The scripts needed to run
+
+    # first, force map to render as HTML, for us to dissect
+    _ = folium_map._repr_html_()
+
+    # get definition of map in body
+    map_div = Markup(folium_map.get_root().html.render())
+
+    # html to be included in header
+    hdr_txt = Markup(folium_map.get_root().header.render())
+
+    # html to be included in <script>
+    script_txt = Markup(folium_map.get_root().script.render())
+    return render_template("model_page.html", map_div=map_div, hdr_txt=hdr_txt, script_txt=script_txt)
+
+
+
+@app.route('/')
+def index():
+    start_coords = (46.9540700, 142.7360300)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return folium_map._repr_html_()
