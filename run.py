049850086ff1888b4e96be93f1e03e0dbccbaf69
aemerick <aemerick11@gmail.com>
2020-10-06 04:43:15 +0000

    host and debug checks for where we are running

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
 from flaskapp import app
-app.run(debug=True)
+
+import os
+
+# this is not great, but for now:
+if 'ubuntu' in os.getcwd(): # we are on AWS
+    host  = '0.0.0.0'
+    debug = False
+else:
+    host  = '127.0.0.1'
+    debug = True
+
+app.run(host = host, debug=debug)
